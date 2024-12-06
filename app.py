08e4b5baa8f42f3593bc3f4517053854c3396dd8
Gabriel Azorín <ga.azorin@gmail.com>
2024-12-06 16:08:06 -0300

    Avances 2024/12/06

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
 import streamlit as st
-import pandas as pd
 from main import *
-
-#Creo variable de interfaz en True para contar con dos estados: cuando es None,
-#se muestra la interfaz normal; cuando sí tiene un archivo, cambia la interfaz (para mostrar las características del planner)
 
 st.title('Asistente revisor de contenidos')
 st.markdown('---')
 st.html('<h2>¿Cómo funciona?</h2>')
-st.html("""<p>Este es un espacio que permite <strong>revisar las publicaciones de redes sociales</strong> 
-        directamente <strong>subiéndole el planner</strong> que actualizan los CMs del equipo.</p>""")
+st.html("""<p>Este es un espacio ayuda a la editora y la coordinadora en la revisión de contenidos del equipo.
+    De momento, sirve para <strong>revisar las publicaciones de redes sociales</strong> 
+    directamente <strong>subiéndole el excel del planner</strong> que actualizan los CMs del equipo (hay que descargarlo y subirlo).</p>""")
+st.image("./download_planner_gif.gif", width=450, caption='Así se descarga un excel de un planner.')
 st.markdown('---')
 st.html('<h2>Sube aquí el planner ⬇</h2>')
 uploaded_file = st.file_uploader("Sube acá el planner que descargaste, el excel", type=["xlsx"])
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 import streamlit as st
+from collections import Counter
 
 def treat_excel(df):
     columns_to_erase = ['Id. de tarea ', 'Priority', 'Fecha de vencimiento', 'Es periódica', 'Con retraso']
@@ -11,7 +11,19 @@ def treat_excel(df):
     st.write("El planner que cargaste:")
     st.write(df.head())
 
-    # Crear la nube de palabras a partir de la columna 'Etiquetas'
+    # Vamos a ver qué etiquetas hay
     if 'Etiquetas' in df.columns:
         etiquetas = df['Etiquetas'].dropna()  # Eliminar valores nulos
-        todas_etiquetas = ','.join(etiquetas)  # Unir todas las etiquetas en un solo string
+    
+        # Crear una lista para almacenar todas las etiquetas
+        lista_etiquetas = []
+        for etiqueta in etiquetas:
+            # Dividir las etiquetas que vienen separadas por comas y eliminar espacios innecesarios
+            lista_etiquetas.extend([e.strip() for e in etiqueta.split(';')])
+        
+        # Contar las veces que aparece cada etiqueta usando Counter
+        conteo_etiquetas = Counter(lista_etiquetas)
+
+        # Mostrar el conteo de etiquetas
+        st.write("Conteo de etiquetas únicas:")
+        st.write(dict(conteo_etiquetas))
